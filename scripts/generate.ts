9bde17b3c4c8397792a8c42184e4021498ce4a09
cole <bernankez@qq.com>
2023-02-13 03:16:57 +0800

    chore: replace `tsxv` with `tsvx`

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -9,8 +9,8 @@ const { success, error } = createConsole("generate types ");
 
 async function generate() {
   await Promise.all([
-    execa("pnpm", ["tsxv", "--tsconfig", resolve(root, "./tsconfig.esbuild.json"), "./scripts/generate-volar-types.ts"], { cwd: root, stdio: "inherit" }),
-    execa("pnpm", ["tsxv", "--tsconfig", resolve(root, "./tsconfig.esbuild.json"), "./scripts/generate-webstorm-types.ts"], { cwd: root, stdio: "inherit" }),
+    execa("pnpm", ["tsvx", "--tsconfig", resolve(root, "./tsconfig.esbuild.json"), "./scripts/generate-volar-types.ts"], { cwd: root, stdio: "inherit" }),
+    execa("pnpm", ["tsvx", "--tsconfig", resolve(root, "./tsconfig.esbuild.json"), "./scripts/generate-webstorm-types.ts"], { cwd: root, stdio: "inherit" }),
   ]).then(() => {
     success(`types generated in ${resolve(root, "./packages/bobo-style")}`);
     return true;
